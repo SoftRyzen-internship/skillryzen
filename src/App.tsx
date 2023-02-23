e000d956cb8ae318615eb251b4b1bc077ffb19f6
egarra <garikhabuzov@gmail.com>
2023-02-23 14:46:49 +0200

    SR-9: interface props change

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,12 @@ import { Layout } from '@modules/dashboard/components/Layout/Layout'
 import AppRoutes from './routes'
 import './theme/styles/global.scss'
 import './theme/styles/variables.scss'
-import { Tabs } from './UIDesign/Tabs/Tabs'
-
-const props = {
-  firstTab: 'All tests',
-  secondTab: 'My tests'
-}
 
 export const App: React.FC = () => {
   return (
     <>
       <Layout>
         <AppRoutes />
-        <Tabs props={props}/>
       </Layout>
     </>
   )
--- src/UIDesign/Tabs/Tabs.tsx
+++ src/UIDesign/Tabs/Tabs.tsx
@@ -1,32 +1,33 @@
-import React, { FC } from 'react'
 import { NavLink } from 'react-router-dom'
 
 import s from './Tabs.module.scss'
 
-export interface IProps {
-  props: {
-    firstTab: string
-    secondTab: string
-  }
+interface ITab {
+  title: string
+  id: string
+  to: string
+}
+interface ITabProps {
+  tabs: ITab[]
 }
 
-export const Tabs: FC<IProps> = ({ props }) => {
+export function Tabs({ tabs }: ITabProps) {
   return (
     <ul className={s.tabs__list}>
-      <li className={s.tabs__item}>
-        <NavLink to='/' className={({ isActive }:{isActive: boolean}) =>
-              isActive ? s.tabs__navLinkActive : s.tabs__navLink
-            }>
-        {props.firstTab}
-        </NavLink>
-      </li>
-      <li className={s.tabs__item}>
-        <NavLink to='' className={({ isActive }:{isActive: boolean}) =>
-              isActive ? s.tabs__navLinkActive : s.tabs__navLink
-            }>
-        {props.secondTab}
-        </NavLink>
-      </li>
+      {tabs.map(({ title, id, to }) => {
+        return (
+          <li className={s.tabs__item} key={id}>
+            <NavLink
+              to={to}
+              className={({ isActive }: { isActive: boolean }) =>
+                isActive ? s.tabs__navLinkActive : s.tabs__navLink
+              }
+            >
+              {title}
+            </NavLink>
+          </li>
+        )
+      })}
     </ul>
   )
 }
