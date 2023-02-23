f693cea69b1618c9e773ccadc4c96a9e73d719e3
Vita Kostiuk <vitagrebennik@gmail.com>
2023-02-23 15:31:28 +0200

    SR-6: add-icon

--- src/UIDesign/Input/Input.tsx
+++ src/UIDesign/Input/Input.tsx
@@ -2,24 +2,30 @@ import { useState } from 'react'
 import s from './Input.module.scss'
 
 interface IProps {
-  className?: string;
-  name: string;
-  placeholder: string;
-  type?: string;
+  className?: string
+  name: string
+  placeholder: string
+  type?: string
+  icon?: string
 }
 
-const Input = ({className, name, placeholder, type}: IProps) => {
+const Input = ({ className, name, placeholder, type, icon }: IProps) => {
   const [inputValue, setInputValue] = useState<string>('')
 
   return (
-    <input
-      className={className ? className : s.input}
-      name={name}
-      placeholder={placeholder}
-      value={inputValue}
-      type={type}
-      onChange={e => setInputValue(e.currentTarget.value.toLowerCase())}
-    />
+    <div className={s.inputContainer}>
+      <input
+        className={className ? className : s.input}
+        name={name}
+        placeholder={placeholder}
+        type={type}
+        value={inputValue}
+        onChange={(e) => setInputValue(e.currentTarget.value.toLowerCase())}
+      />
+      <button className={icon ? s.iconVisible : s.iconHidden} type="button">
+        <img className={s.icon} src={icon} />
+      </button>
+    </div>
   )
 }
 
