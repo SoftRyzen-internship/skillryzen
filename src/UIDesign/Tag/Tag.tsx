82b8d28be548b7e109c0ba8383b46be90589bd64
GubarievaAnna <annagubarieva@gmail.com>
2023-02-23 16:37:01 +0200

    SR-3: Add variables for fonts

--- src/UIDesign/Tag/Tag.tsx
+++ src/UIDesign/Tag/Tag.tsx
@@ -4,23 +4,19 @@ import s from './Tag.module.scss'
 interface ITag {
   type: 'field' | 'questions' | 'time'
   label: string
-  icon?: boolean  
+  icon?: boolean
 }
 
 export const Tag: React.FC<ITag> = ({ label, type, icon = false }) => {
   return (
-      <p
-        className={`${type !== 'time' && s["tag--common"]} ${s[`tag--${type}`]} ${
-          icon && s["tag--icon"]
-        } `}
-      >
-        {type === "time" && <span className={s.label}>{label} год</span>}
-        {type !== "time" && label} {type === 'questions' && 'запитань'}
-        {icon && (
+    <p className={`${s[`tag--${type}`]} ${icon && s['tag--icon']} `}>
+      {type === 'time' && <span className={s.label}>{label} год</span>}
+      {type !== 'time' && label} {type === 'questions' && 'запитань'}
+      {icon && (
         <svg>
           <use href="" width="12" height="12"></use>
         </svg>
-      )} 
-      </p>
+      )}
+    </p>
   )
 }
