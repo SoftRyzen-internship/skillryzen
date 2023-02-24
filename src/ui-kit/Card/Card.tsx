2f018347dbe373f25c0cf0285b346e9c96d8d630
GubarievaAnna <annagubarieva@gmail.com>
2023-02-24 12:59:05 +0200

    SR-5: Add images and custom card

---
+++ src/ui-kit/Card/Card.tsx
@@ -0,0 +1,122 @@
+import React from 'react'
+import { Tag } from '@ui-kit/Tag'
+import { ICONS } from '@theme/icons.const'
+import s from './Card.module.scss'
+
+interface ICard {
+  type?: 'notification' | 'coin' | 'info'
+  size?: 'large' | 'small'
+  hideNumber?: boolean
+  custom?: boolean
+  onClick?: () => void
+  item: IItem
+}
+
+interface IItem {
+  title: string
+  text?: string
+  fields?: string[]
+  number?: number
+  time?: number
+}
+
+export const Card: React.FC<ICard> = ({
+  type = 'info',
+  item,
+  size = 'large',
+  hideNumber = false,
+  custom = false,
+  onClick,
+}) => {
+  const { title, text, fields, number, time } = item
+
+  const addBackgroundColor = (type: string): string => {
+    if (type === 'notification' || custom) {
+      return 'green'
+    }
+    if (type === 'coin') {
+      return 'blue'
+    }
+    return 'yellow'
+  }
+
+  const addIcon = (type: string): string => {
+    if (type === 'notification') {
+      return ICONS.WARNING_CIRCLE
+    }
+    if (type === 'coin') {
+      return ICONS.COIN
+    }
+    if (custom) {
+      return ICONS.EDIT
+    }
+    return ICONS.WARNING_CIRCLE
+  }
+
+  const addDescription = (type: string): string => {
+    if (type === 'notification') {
+      return "warning"
+    }
+    if (type === 'coin') {
+      return "coin"
+    }
+    if (custom) {
+      return "edit"
+    }
+    return "lock"
+  }
+
+  return (
+    <li className={`${s[`card--${size}`]}`}>
+      <div className={s.card__infoWrapper}>
+        <div className={`${s[`card__thumb--${addBackgroundColor(type)}`]}`}>
+        <img src={addIcon(type)} width="18" height="20" alt={addDescription(type)}/>
+        </div>
+        {!custom && (
+          <div className={s.card__contentWrapper}>
+            <div className={s.card__content}>
+              <p className={s.card__title}>{title}</p>
+              <p className={s.card__text}>{text}</p>
+            </div>
+            {fields && type === 'info' && (
+              <ul className={s.card__list}>
+                {fields.map((item, index) => (
+                  <li key={index}>
+                    <Tag type="field" label={item} />
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+        )}
+        {custom && <p className={s.card__template}>{title}</p>}
+      </div>
+      <div
+        className={`${s.card__addInfoWrapper} ${
+          type !== 'info' && s['card__addInfoWrapper--notification']
+        }`}
+      >
+        {((!hideNumber && number) || custom) && (
+          <Tag
+            type="number"
+            label={
+              type === 'info' && custom
+                ? '? запитань'
+                : !custom
+                ? number + ' запитань'
+                : number + ' min ago'
+            }
+          />
+        )}
+        {((time && type === 'info') || custom) && (
+          <Tag type="time" label={!custom ? time + '' : '?'} />
+        )}
+        {type !== 'info' && (
+          <button className={s.card__close} onClick={onClick}>
+            <img src={ICONS.CLOSE} width="8" height="8" alt="close"/>
+          </button>
+        )}
+      </div>
+    </li>
+  )
+}
--- src/ui-kit/Tag/Tag.tsx
+++ src/ui-kit/Tag/Tag.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ICONS } from '@theme/icons.const'
 import s from './Tag.module.scss'
 
 interface ITag {
@@ -12,11 +13,7 @@ export const Tag: React.FC<ITag> = ({ label, type, icon = false }) => {
     <p className={`${s[`tag--${type}`]} ${icon && s['tag--icon']} `}>
       {type === 'time' && <span className={s.label}>{label} год</span>}
       {type !== 'time' && label}
-      {icon && (
-        <svg>
-          <use href="" width="12" height="12"></use>
-        </svg>
-      )}
+      {icon && <img src={ICONS.CLOSE} width="12" height="12" alt="close"/>}
     </p>
   )
 }
