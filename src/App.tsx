7fa37227c5546de70b01d3ae080bc2b147cec5ef
egarra <garikhabuzov@gmail.com>
2023-02-24 12:57:38 +0200

    SR-41: update app

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,10 @@ import { AppRoutes } from './routes'
 import './theme/styles/global.scss'
 import './theme/styles/variables.scss'
 
-import { Tabs } from './ui-kit'
-
-const tabs = [
-  {
-    title: 'smth',
-    path: '/route'
-  },
-  {
-    title: 'else',
-    path: '/'
-  }
-]
-
 export const App: React.FC = () => {
   return (
     <>
       <Layout>
-        <Tabs tabs={tabs}/>
         <AppRoutes />
         <Suspense fallback={<p>Loading..</p>}>
           <AppRoutes />
