188241a3452c835214ec3ec87cb857992995ca3b
GubarievaAnna <annagubarieva@gmail.com>
2023-02-24 15:20:04 +0200

    SR-1: Add timer

---
+++ src/modules/common/components/Timer/Timer.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import s from './Timer.module.scss'
+
+interface ITimer {
+  time: number;
+}
+
+export const Timer: React.FC<ITimer> = ({ time }) => {
+    const [seconds, setSeconds] = useState(time*3600);
+
+    useEffect(() => {
+      if (seconds > 0) {
+        const intervalId = setInterval(() => {
+          setSeconds(seconds => seconds - 1);
+        }, 1000);
+        return () => clearInterval(intervalId);
+      }
+    }, [seconds]);
+  
+    const convertTime = (time: number) => {
+      const minutes = Math.floor(time / 60).toString().padStart(2, '0');
+      const seconds = (time % 60).toString().padStart(2, '0');
+      return `${minutes}:${seconds}`;
+    };
+  
+    return (
+      <div>
+        <p className={s.timer__text}>Time left:</p>
+        <p className={s.timer__time}>{convertTime(seconds)}</p>
+      </div>
+    );
+  }
+
+
+
