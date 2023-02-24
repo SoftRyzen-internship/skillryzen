1387c76e8ceb5e4b03d0a8c28f89bbd6d79a3f73
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-02-24 07:31:15 +0200

    SR-34: init routes and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { Suspense } from 'react'
+
 import { Layout } from '@modules/dashboard/components/Layout/Layout'
-import AppRoutes from './routes'
+import { AppRoutes } from './routes'
+
 import './theme/styles/global.scss'
 import './theme/styles/variables.scss'
 
@@ -7,9 +10,10 @@ export const App: React.FC = () => {
   return (
     <>
       <Layout>
-        <AppRoutes />
+        <Suspense fallback={<p>Loading..</p>}>
+          <AppRoutes />
+        </Suspense>
       </Layout>
     </>
   )
 }
-
