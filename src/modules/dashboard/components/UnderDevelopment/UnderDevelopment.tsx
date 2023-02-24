0f48e5913b7b110a81761bc406ae7438fb1adb8a
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2023-02-24 18:02:39 +0200

    SR-22: Add wrapper

--- src/modules/dashboard/components/UnderDevelopment/UnderDevelopment.tsx
+++ src/modules/dashboard/components/UnderDevelopment/UnderDevelopment.tsx
@@ -9,8 +9,10 @@ export const UnderDevelopment: React.FC<UnderDevelopmentProps> = ({
   title,
 }) => {
   return (
-    <section className={s.section}>
-      <h2 className={s.title}>{title}</h2>
+    <section>
+      <div className={s.wrapper}>
+        <h2 className={s.title}>{title}</h2>
+      </div>
     </section>
   );
 };
