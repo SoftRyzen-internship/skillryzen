20867b7fa084bb1ab1244917a36b92b3d3df1c14
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2023-02-24 17:42:02 +0200

    SR-22: Update markup and styles

--- src/modules/dashboard/components/UnderDevelopment/UnderDevelopment.tsx
+++ src/modules/dashboard/components/UnderDevelopment/UnderDevelopment.tsx
@@ -9,8 +9,8 @@ export const UnderDevelopment: React.FC<UnderDevelopmentProps> = ({
   title,
 }) => {
   return (
-    <div className={s.background}>
-      <h1 className={s.title}>{title}</h1>
-    </div>
+    <section className={s.section}>
+      <h2 className={s.title}>{title}</h2>
+    </section>
   );
 };
