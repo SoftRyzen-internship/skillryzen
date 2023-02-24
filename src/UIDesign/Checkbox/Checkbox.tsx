cb3253119ced5861545a36dc5db44b3c4743a1c6
Oleksandr Nikolaienko <aleksander.nikolaenko@gmail.com>
2023-02-24 08:27:25 +0200

    SR-10: Add styles checkbox

--- src/UIDesign/Checkbox/Checkbox.tsx
+++ src/UIDesign/Checkbox/Checkbox.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ICONS } from '../../theme/icons.const'
 
 import s from './Checkbox.module.scss'
 
@@ -18,7 +19,14 @@ export default function Checkbox({
   return (
     <label className={`${labelClassName} ${s.label}`}>
       <input type="checkbox" checked={checked} onChange={onChange} />
-      <span className={s.checkbox}></span>
+      <span className={s.checkbox}>
+        <img
+          src={checked ? ICONS.CHECK_ROUND : ICONS.DEFAULT_CHECKBOX}
+          width={16}
+          height={16}
+          alt="checkbox icon"
+        />
+      </span>
       {label}
     </label>
   )
--- src/modules/pages/Home/Home.tsx
+++ src/modules/pages/Home/Home.tsx
@@ -1,12 +1,5 @@
-import CheckboxExample from '../../../UIDesign/Checkbox/example/CheckboxExample'
-
 const Home = () => {
-  return (
-    <>
-      <div>Home test</div>
-      <CheckboxExample />
-    </>
-  )
+  return <div>Home test</div>
 }
 
 export default Home
