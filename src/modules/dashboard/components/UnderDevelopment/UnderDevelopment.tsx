01cc141bfbd0facce2bf00eb0881f947b12e5194
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2023-02-24 18:13:24 +0200

    SR-22: Update styles on section

--- src/modules/dashboard/components/UnderDevelopment/UnderDevelopment.tsx
+++ src/modules/dashboard/components/UnderDevelopment/UnderDevelopment.tsx
@@ -9,7 +9,7 @@ export const UnderDevelopment: React.FC<UnderDevelopmentProps> = ({
   title,
 }) => {
   return (
-    <section>
+    <section className={s.section}>
       <div className={s.wrapper}>
         <h2 className={s.title}>{title}</h2>
       </div>
