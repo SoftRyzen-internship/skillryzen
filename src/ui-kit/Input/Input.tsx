deff1a1c29fad5df27aba09d905e610e1e1dfe32
Vita Kostiuk <vitagrebennik@gmail.com>
2023-02-24 12:15:56 +0200

    SR-6: fix-styles-and-create-function

--- src/ui-kit/Input/Input.tsx
+++ src/ui-kit/Input/Input.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+
 import s from './Input.module.scss'
 
 interface IProps {
@@ -9,8 +10,12 @@ interface IProps {
   icon?: string
 }
 
-const Input = ({ className, name, placeholder, type, icon }: IProps) => {
+export const Input = ({ className, name, placeholder, type, icon }: IProps) => {
   const [inputValue, setInputValue] = useState<string>('')
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value.toLowerCase().trim())
+  }
 
   return (
     <div className={s.inputContainer}>
@@ -20,13 +25,11 @@ const Input = ({ className, name, placeholder, type, icon }: IProps) => {
         placeholder={placeholder}
         type={type}
         value={inputValue}
-        onChange={(e) => setInputValue(e.currentTarget.value.toLowerCase())}
+        onChange={handleInputChange}
       />
       <button className={icon ? s.iconVisible : s.iconHidden} type="button">
-        <img className={s.icon} src={icon} />
+        <img className={s.icon} src={icon} alt="Icon Search" />
       </button>
     </div>
   )
 }
-
-export default Input
