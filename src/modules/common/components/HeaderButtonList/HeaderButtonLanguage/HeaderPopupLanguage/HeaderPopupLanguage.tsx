70e5c1ae869627e3e8a3f190cfa2a9d74f25b322
Oleksandr Nikolaienko <aleksander.nikolaenko@gmail.com>
2023-02-27 16:10:54 +0200

    SR-3: Delete comments

--- src/modules/common/components/HeaderButtonList/HeaderButtonLanguage/HeaderPopupLanguage/HeaderPopupLanguage.tsx
+++ src/modules/common/components/HeaderButtonList/HeaderButtonLanguage/HeaderPopupLanguage/HeaderPopupLanguage.tsx
@@ -1,17 +1,3 @@
-// import { ICONS } from '@theme/icons.const'
-// import { Popup } from '@ui-kit/index'
-
 export const HeaderPopupLanguage = () => {
   return <div>Popup Language</div>;
-  // const popupList = [
-  //   {
-  //     icon: ICONS.C_UK,
-  //     text: 'English',
-  //   },
-  //   {
-  //     icon: ICONS.C_UKRAINE,
-  //     text: 'Ukrainian',
-  //   },
-  // ]
-  // return <Popup list={popupList} />
 };
