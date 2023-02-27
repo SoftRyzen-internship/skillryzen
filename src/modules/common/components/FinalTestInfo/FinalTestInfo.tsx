d48b76cdaa516a15af30ca32f9702ec3e9a6e735
Vita Kostiuk <vitagrebennik@gmail.com>
2023-02-27 16:32:40 +0200

    SR-32: add-layout-for-start-and-end-test

--- src/modules/common/components/FinalTestInfo/FinalTestInfo.tsx
+++ src/modules/common/components/FinalTestInfo/FinalTestInfo.tsx
@@ -1,38 +1,158 @@
 import React from 'react';
-import { IMGS } from '../../../../theme/images.const';
 
-export const FinalTestInfo = () => {
+interface IListInfoJS {
+  topics: string;
+  time: string;
+  questions: number;
+  learners: number;
+  author: string;
+}
+
+interface IIconProps {
+  alt: string;
+  width: string;
+  height: string;
+}
+
+interface IProps {
+  icon: string;
+  iconProps: IIconProps;
+  title: string;
+  subtitle?: string;
+  listInfo?: IListInfoJS;
+  correctAnswers?: number;
+  totalQuestions?: number;
+  timeSpent?: number;
+  theWorstTopic?: string;
+  theBestTopic?: string;
+  onClickBtn: () => void;
+  textBtn: string;
+  finishTest?: boolean;
+  test: string;
+}
+
+export const FinalTestInfo = ({
+  icon,
+  iconProps,
+  title,
+  subtitle,
+  listInfo,
+  correctAnswers,
+  totalQuestions,
+  timeSpent,
+  theWorstTopic,
+  theBestTopic,
+  onClickBtn,
+  textBtn,
+  finishTest,
+  test,
+}: IProps) => {
+  const { alt, width, height } = iconProps;
   return (
     <>
-      {' '}
-      <img src={IMGS.JAVA_SCRIPT} alt='Java Script' width='146' height='146' />
-      <h2>FullStack - Final Test</h2>
-      <p>
-        JavaScript is a programming language that is one of the core
-        technologies of the World Wide Web, alongside HTML and CSS.{' '}
-      </p>
-      <ul>
-        <li>
-          <p>Теми</p>
-          <p>HTML, CSS, JavaScript, React</p>
-        </li>
-        <li>
-          <p>Час проходження</p>
-          <p>2:00:00</p>
-        </li>
-        <li>
-          <p>Питання</p>
-          <p>50</p>
-        </li>
-        <li>
-          <p>Learners</p>
-          <p>200</p>
-        </li>
-        <li>
-          <p>Author</p>
-          <p>GoIT</p>
-        </li>
-      </ul>
+      <img src={icon} alt={alt} width={width} height={height} />
+      <h2>{title}</h2>
+      {subtitle && <p>{subtitle}</p>}
+      {listInfo ? (
+        <ul>
+          <li>
+            <p>Теми</p>
+            <p>{`${listInfo.topics}`}</p>
+          </li>
+          <li>
+            <p>Час проходження</p>
+            <p>{`${listInfo.time}`}</p>
+          </li>
+          <li>
+            <p>Питання</p>
+            <p>{`${listInfo.questions}`}</p>
+          </li>
+          <li>
+            <p>Learners</p>
+            <p>{`${listInfo.learners}`}</p>
+          </li>
+          <li>
+            <p>Author</p>
+            <p>{`${listInfo.author}`}</p>
+          </li>
+        </ul>
+      ) : (
+        ''
+      )}
+
+      {finishTest && (
+        <>
+          <ul>
+            <li>
+              <p>Correct answers</p>
+              <p>
+                {`${correctAnswers}`}/{`${totalQuestions}`}
+              </p>
+            </li>
+            <li>
+              <p>Time spent</p>
+              <p>{`${timeSpent}`} min</p>
+            </li>
+          </ul>
+          <div>
+            <p>Recomendations:</p>
+            <p>
+              The worst topic in {`${test}`} - {`${theWorstTopic}`}
+            </p>
+            <p>
+              The best topic in {`${test}`} - {`${theBestTopic}`}
+            </p>
+          </div>
+        </>
+      )}
+
+      <button type='button' onClick={onClickBtn}>
+        {textBtn}
+      </button>
     </>
   );
 };
+
+// Приклад використання в компоненті
+
+// const listInfoJS = {
+//   topics: 'HTML, CSS, JavaScript, React',
+//   time: '2:00:00',
+//   questions: 50,
+//   learners: 200,
+//   author: 'GoIT',
+// };
+
+// const [isClickBtn, setIsClickBtn] = useState(false);
+// const handleClickBtn = () => {
+//   setIsClickBtn(true);
+// };
+
+// PROPS For Start Test
+// <FinalTestInfo
+//   icon={IMGS.JAVA_SCRIPT}
+//   iconProps={{ alt: 'Java Script', width: '146', height: '146' }}
+//   title='FullStack - Final Test'
+//   subtitle='JavaScript is a programming language that is one of the core
+//     technologies of the World Wide Web, alongside HTML and CSS.'
+//   listInfo={listInfoJS}
+//   onClickBtn={handleClickBtn}
+//   textBtn='Start test'
+//   test = 'JS';
+// />;
+
+// PROPS For End Test
+// <FinalTestInfo
+//   icon={IMGS.JAVA_SCRIPT}
+//   iconProps={{ alt: 'Java Script', width: '146', height: '146' }}
+//   title='FullStack - Final Test'
+//   correctAnswers={15}
+//   totalQuestions={50}
+//   timeSpent={20}
+//   theWorstTopic='The worst topic in JS - “Asynchrony” section.'
+//   theBestTopic='The best topic in JS - "Lorem lorem lorem" section.'
+//   onClickBtn={handleClickBtn}
+//   textBtn='Start test'
+//   test='JS'
+//   finishTest
+// />;
--- src/modules/pages/LoginPage/LoginPage.tsx
+++ src/modules/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,46 @@
+import { useState } from 'react';
 import { FinalTestInfo } from '../../common/components/FinalTestInfo/FinalTestInfo';
+import { IMGS } from '../../../theme/images.const';
 
+const listInfoJS = {
+  topics: 'HTML, CSS, JavaScript, React',
+  time: '2:00:00',
+  questions: 50,
+  learners: 200,
+  author: 'GoIT',
+};
 const LoginPage = () => {
-  return <FinalTestInfo />;
+  const [isClickBtn, setIsClickBtn] = useState(false);
+  const handleClickBtn = () => {
+    setIsClickBtn(true);
+  };
+  return (
+    <FinalTestInfo
+      icon={IMGS.JAVA_SCRIPT}
+      iconProps={{ alt: 'Java Script', width: '146', height: '146' }}
+      title='FullStack - Final Test'
+      subtitle='JavaScript is a programming language that is one of the core
+        technologies of the World Wide Web, alongside HTML and CSS.'
+      listInfo={listInfoJS}
+      onClickBtn={handleClickBtn}
+      textBtn='Start test'
+      test='JS'
+    />
+    // <FinalTestInfo
+    //   icon={IMGS.JAVA_SCRIPT}
+    //   iconProps={{ alt: 'Java Script', width: '146', height: '146' }}
+    //   title='FullStack - Final Test'
+    //   correctAnswers={15}
+    //   totalQuestions={50}
+    //   timeSpent={20}
+    //   theWorstTopic='The worst topic in JS - “Asynchrony” section.'
+    //   theBestTopic='The best topic in JS - "Lorem lorem lorem" section.'
+    //   onClickBtn={handleClickBtn}
+    //   textBtn='Start test'
+    //   test='JS'
+    //   finishTest
+    // />
+  );
 };
 
 export default LoginPage;
