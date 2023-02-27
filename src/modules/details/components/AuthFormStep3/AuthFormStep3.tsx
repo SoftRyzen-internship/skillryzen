992888bbb4bd0f5d08064aeed82b19d9f2c364b1
Shynal-Nazar <87614426+Shynal-Nazar@users.noreply.github.com>
2023-02-27 17:27:11 +0200

    only form in element

--- src/modules/details/components/AuthFormStep3/AuthFormStep3.tsx
+++ src/modules/details/components/AuthFormStep3/AuthFormStep3.tsx
@@ -4,30 +4,27 @@ import { AuthButton, Input } from '../../../../ui-kit/index';
 export const AuthFormStep3 = () => {
   const handleClick = () => {};
   return (
-    <div className={s.contentrapper}>
-      <h2 className={s.formTitle}>Give us more information about you! </h2>
-      <p className={s.formText}> Lorem lorem</p>
-      <form action=''>
-        <ul>
-          <li className={s.list__item}>
-            <Input name='name' placeholder='Name'></Input>
-          </li>
-          <li className={s.list__item}>
-            <Input name='surname' placeholder='Surname'></Input>
-          </li>
-          <li className={s.list__item}>
-            <Input name='Phone' placeholder='Phone'></Input>
-          </li>
-        </ul>
+    <form action=''>
+      <ul>
+        <li className={s.list__item}>
+          <Input name='name' placeholder='Name' />
+          <Input name='name' placeholder='Name'></Input>
+        </li>
+        <li className={s.list__item}>
+          <Input name='surname' placeholder='Surname'></Input>
+        </li>
+        <li className={s.list__item}>
+          <Input name='Phone' placeholder='Phone'></Input>
+        </li>
+      </ul>
 
-        <AuthButton
-          type='submit'
-          text='Continue'
-          size='small'
-          color='grey'
-          onClick={handleClick}
-        />
-      </form>
-    </div>
+      <AuthButton
+        type='submit'
+        text='Continue'
+        size='small'
+        color='grey'
+        onClick={handleClick}
+      />
+    </form>
   );
 };
