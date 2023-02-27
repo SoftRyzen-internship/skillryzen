7cd062eeab5eca07f0b01b9561b54cd555bda0c7
Vita Kostiuk <vitagrebennik@gmail.com>
2023-02-27 13:38:12 +0200

    SR-6: remove-tests

--- src/modules/pages/LoginPage/LoginPage.tsx
+++ src/modules/pages/LoginPage/LoginPage.tsx
@@ -1,8 +1,5 @@
-import { Input } from '../../../ui-kit/Input/Input';
-
 const LoginPage = () => {
-  // return <div>Login</div>;
-  return <Input name='search' placeholder='Search' button />;
+  return <div>Login</div>;
 };
 
 export default LoginPage;
