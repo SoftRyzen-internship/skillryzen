a786e3c4a9e6025162152ce9b6a0a8a947ac9e02
Shynal-Nazar <87614426+Shynal-Nazar@users.noreply.github.com>
2023-02-27 13:05:10 +0200

    SR-7 Change AuthBTN
    додає властивість disabled на кнопку для зміни кольору на синій та сірий кольори

--- src/ui-kit/Buttons/AuthBtn/AuthButton.tsx
+++ src/ui-kit/Buttons/AuthBtn/AuthButton.tsx
@@ -5,7 +5,8 @@ interface ButtonProps {
   text: string;
   onClick: () => void;
   size: 'small' | 'large';
-  color: 'grey' | 'white' | 'blue';
+  disabled?: boolean; //коли disabled то кнопка сіра, в іншому випадку акцент колір
+  needBackground?: 'noBackgroundAccent' | 'noBackgroundGray'; //робить кнопку прозорою, та додає акцент колір на бордер та текст або сірий бордер
 }
 
 export const AuthButton = ({
@@ -13,13 +14,15 @@ export const AuthButton = ({
   text,
   onClick,
   size = 'small',
-  color,
+  disabled,
+  needBackground,
 }: ButtonProps) => {
   return (
     <button
+      disabled={disabled}
       onClick={onClick}
       type={type}
-      className={`${s.authBtn} ${s[size]} ${s[color]}`}
+      className={`${s.authBtn} ${s[size]} ${s[needBackground]}`}
     >
       {text}
     </button>
