a00294ed8f11495abb997c0b20b7a888141f8543
Vita Kostiuk <vitagrebennik@gmail.com>
2023-02-27 13:36:35 +0200

    SR-6: add-button-prop

--- src/modules/pages/LoginPage/LoginPage.tsx
+++ src/modules/pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,8 @@
+import { Input } from '../../../ui-kit/Input/Input';
+
 const LoginPage = () => {
-  return <div>Login</div>;
+  // return <div>Login</div>;
+  return <Input name='search' placeholder='Search' button />;
 };
 
 export default LoginPage;
