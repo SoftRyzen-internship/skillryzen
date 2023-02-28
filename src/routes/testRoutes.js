475fe8f7ca7c0e852f2a6edf901027cbbeb07d46
Oleksandr Nikolaienko <aleksander.nikolaenko@gmail.com>
2023-02-28 19:29:26 +0200

    SR-32: fix path in test routes

--- src/routes/testRoutes.js
+++ src/routes/testRoutes.js
@@ -2,8 +2,12 @@ export const testRoutes = [
   { id: 'id-1', name: 'home', to: '/' },
   { id: 'id-2', name: 'login', to: '/login' },
   { id: 'id-3', name: 'register', to: '/register' },
-  { id: 'id-4', name: 'student', to: '/student' },
+  // { id: 'id-4', name: 'student', to: '/student' },
   { id: 'id-5', name: 'company', to: '/company' },
-  { id: 'id-6', name: 'tests', to: '/tests' },
-  { id: 'id-7', name: 'testing', to: '/testing' },
+  { id: 'id-6', name: 'student certification', to: '/student/certification' },
+  {
+    id: 'id-7',
+    name: 'student certification testing',
+    to: '/student/testing',
+  },
 ];
