1990ad936afd6af17436b46a33bd51c74cef1764
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2023-02-28 16:29:58 +0200

    SR-33: Update button, change styles

--- src/modules/common/components/TestQuestion/TestQuestion.tsx
+++ src/modules/common/components/TestQuestion/TestQuestion.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 
 import { AuthButton } from 'ui-kit/Buttons/AuthBtn/AuthButton';
+import { RadioButton } from 'ui-kit';
 
 import s from './TestQuestion.module.scss';
-import { RadioButton } from 'ui-kit';
 
 interface ITestQuestionProps {
   questionId: string;
@@ -38,43 +38,41 @@ export const TestQuestion = ({
     <div className={s.testWrapper}>
       <h2 className={s.testTitle}>{title}</h2>
       <div className={s.questionWrapper}>
-        <div className={s.questionCode}>Code</div>
+        {/* Наступний рядок потрібен коли з'являться питання з кодом */}
+        {/* <div className={s.questionCode}>Code</div> */}
         <ul className={s.questionList}>
           {possibleAnswers.map((answer) => (
             <li className={s.questionItem} key={answer.label}>
-              {/* label, radiobutton треба повністю переробляти
-							тому всі стилі не додані, лише загальні для відображення */}
-              {/* <label htmlFor={answer.label} className={s.questionLabel}>
+              <label className={s.questionLabel}>
                 <input
                   type='radio'
                   name={questionId}
-                  id={answer.label}
                   value={answer.value}
                   checked={selectedAnswer === answer.value}
                   onChange={(e) => setSelectedAnswer(e.target.value)}
                 />
                 {answer.title}
-              </label> */}
-              <RadioButton
+              </label>
+              {/* <RadioButton
                 value={answer.value}
                 checked={selectedAnswer === answer.value}
                 onChange={(e) => setSelectedAnswer(e.target.value)}
-                labelClassName={s.questionLabel}
+                // labelClassName={s.questionLabel}
                 label={answer.title}
-              />
+              /> */}
             </li>
           ))}
         </ul>
       </div>
-      {/* кнопку требе доопрацювати */}
-      <AuthButton
-        type='button'
-        text='Answer'
-        onClick={handleAnswer}
-        size='small'
-        color='blue'
-        // disabled={!selectedAnswer}
-      />
+      <div className={s.buttonWrapper}>
+        <AuthButton
+          type='button'
+          text='Answer'
+          onClick={handleAnswer}
+          size='small'
+          disabled={!selectedAnswer}
+        />
+      </div>
       {!hasNextQuestion && <p>The end(here must be next page)</p>}
     </div>
   );
