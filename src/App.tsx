c6863f00db22e88e6c9f7560e63c70de8f7267b3
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2023-02-28 10:55:04 +0200

    SR-22: Update img path, change props

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { AppRoutes } from './routes';
 
 import './theme/styles/global.scss';
 import './theme/styles/variables.scss';
+import { UnderDevelopment } from 'modules/dashboard/components/UnderDevelopment/UnderDevelopment';
 
 export const App = () => {
   return (
     <>
       <Suspense fallback={<p>Loading..</p>}>
         <AppRoutes />
+        <UnderDevelopment title='he' />
       </Suspense>
     </>
   );
