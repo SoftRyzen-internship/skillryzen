015ee35bb8556492f0f18cf7f98dc914a0322f72
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2023-03-01 09:32:25 +0200

    SR-33: Change logic and types

--- src/modules/common/components/TestQuestion/TestQuestion.tsx
+++ src/modules/common/components/TestQuestion/TestQuestion.tsx
@@ -3,9 +3,13 @@ import React, { useState } from 'react';
 import { AuthButton } from 'ui-kit/Buttons/AuthBtn/AuthButton';
 import { RadioButton } from 'ui-kit';
 
+import { array } from 'pages/TestingPage/TestingPage';
+import { IInfo } from 'pages/TestingPage/TestingPage';
+
 import s from './TestQuestion.module.scss';
 
 interface ITestQuestionProps {
+  testId: string;
   questionId: string;
   title: string;
   rightAnswer: string;
@@ -14,10 +18,12 @@ interface ITestQuestionProps {
     title: string;
     label: string;
   }[];
-  onNextQuestion: () => void;
+  onNextQuestion: React.Dispatch<React.SetStateAction<IInfo>>;
   hasNextQuestion: boolean;
 }
+
 export const TestQuestion = ({
+  testId,
   questionId,
   rightAnswer,
   title,
@@ -25,6 +31,7 @@ export const TestQuestion = ({
   onNextQuestion,
   hasNextQuestion,
 }: ITestQuestionProps) => {
+  const [number, setNumber] = useState(1);
   const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null);
   const [sendAnswer, setSendAnswer] = useState<string | null>(null);
 
@@ -34,9 +41,16 @@ export const TestQuestion = ({
       setTimeout(() => {
         setSelectedAnswer('');
         setSendAnswer('');
-        onNextQuestion();
+        onNextQuestion({
+          questionId: array[number].questionId,
+          title: array[number].title,
+          rightAnswer: array[number].rightAnswer,
+          possibleAnswers: array[number].possibleAnswers,
+          hasNextQuestion: array[number].hasNextQuestion,
+        });
       }, 1000);
     }
+    setNumber((prevstate) => prevstate + 1);
   };
 
   return (
@@ -46,27 +60,29 @@ export const TestQuestion = ({
         {/* <div className={s.questionCode}>Code</div> */}
         <ul className={s.questionList}>
           {possibleAnswers.map((answer, index) => (
-            <RadioButton
-              key={index}
-              state={
-                (sendAnswer &&
-                  sendAnswer === answer.value &&
-                  rightAnswer === answer.value) ||
-                (sendAnswer && rightAnswer === answer.value)
-                  ? 'right'
-                  : sendAnswer && sendAnswer === answer.value
-                  ? 'wrong'
-                  : !sendAnswer && selectedAnswer === answer.value
-                  ? 'checked'
-                  : ''
-              }
-              type='PassTest'
-              name='answers'
-              value={answer.value}
-              checked={selectedAnswer === answer.value}
-              onChange={(e) => setSelectedAnswer(e.target.value)}
-              label={answer.value}
-            />
+            <li>
+              <RadioButton
+                key={index}
+                state={
+                  (sendAnswer &&
+                    sendAnswer === answer.value &&
+                    rightAnswer === answer.value) ||
+                  (sendAnswer && rightAnswer === answer.value)
+                    ? 'right'
+                    : sendAnswer && sendAnswer === answer.value
+                    ? 'wrong'
+                    : !sendAnswer && selectedAnswer === answer.value
+                    ? 'checked'
+                    : ''
+                }
+                type='PassTest'
+                name='answers'
+                value={answer.value}
+                checked={selectedAnswer === answer.value}
+                onChange={(e) => setSelectedAnswer(e.target.value)}
+                label={answer.value}
+              />
+            </li>
           ))}
         </ul>
       </div>
--- src/pages/TestingPage/TestingPage.tsx
+++ src/pages/TestingPage/TestingPage.tsx
@@ -1,61 +1,88 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { TestQuestion } from 'modules/common/components/TestQuestion/TestQuestion';
 import { HeaderTest } from 'modules/dashboard';
 
-const TestingPage = () => {
-  const [number, setNumber] = useState(0);
+export const array = [
+  {
+    questionId: 'hjbnmkjlm,',
+    title: 'Що таке Javascript?',
+    rightAnswer: 'мова програмування',
+    possibleAnswers: [
+      { value: 'мова програмування', title: '', label: 'А' },
+      { value: 'бог знає що', title: '', label: 'B' },
+      { value: 'мова життя', title: '', label: 'C' },
+      { value: 'не знаю що', title: '', label: 'D' },
+    ],
+    hasNextQuestion: true,
+  },
+  {
+    questionId: 'hjbnmkjlm,',
+    title: 'Що таке Python?',
+    rightAnswer: 'мова програмування',
+    possibleAnswers: [
+      { value: 'мова програмування', title: '', label: 'А' },
+      { value: 'бог знає що', title: '', label: 'B' },
+      { value: 'мова життя', title: '', label: 'C' },
+      { value: 'не знаю що', title: '', label: 'D' },
+    ],
+    hasNextQuestion: true,
+  },
+  {
+    questionId: 'hjbnmkjlm,',
+    title: 'Що таке C++?',
+    rightAnswer: 'мова програмування',
+    possibleAnswers: [
+      { value: 'мова програмування', title: '', label: 'А' },
+      { value: 'бог знає що', title: '', label: 'B' },
+      { value: 'мова життя', title: '', label: 'C' },
+      { value: 'не знаю що', title: '', label: 'D' },
+    ],
+    hasNextQuestion: true,
+  },
+];
 
-  const array = [
-    {
-      questionId: 'hjbnmkjlm,',
-      title: 'Що таке Javascript?',
-      rightAnswer: 'мова програмування',
-      possibleAnswers: [
-        { value: 'мова програмування', title: '', label: 'А' },
-        { value: 'бог знає що', title: '', label: 'B' },
-        { value: 'мова життя', title: '', label: 'C' },
-        { value: 'не знаю що', title: '', label: 'D' },
-      ],
-      hasNextQuestion: true,
-    },
-    {
-      questionId: 'hjbnmkjlm,',
-      title: 'Що таке Python?',
-      rightAnswer: 'мова програмування',
-      possibleAnswers: [
-        { value: 'мова програмування', title: '', label: 'А' },
-        { value: 'бог знає що', title: '', label: 'B' },
-        { value: 'мова життя', title: '', label: 'C' },
-        { value: 'не знаю що', title: '', label: 'D' },
-      ],
-      hasNextQuestion: true,
-    },
-    {
-      questionId: 'hjbnmkjlm,',
-      title: 'Що таке C++?',
-      rightAnswer: 'мова програмування',
-      possibleAnswers: [
-        { value: 'мова програмування', title: '', label: 'А' },
-        { value: 'бог знає що', title: '', label: 'B' },
-        { value: 'мова життя', title: '', label: 'C' },
-        { value: 'не знаю що', title: '', label: 'D' },
-      ],
-      hasNextQuestion: true,
-    },
-  ];
+export interface IInfo {
+  questionId: string;
+  title: string;
+  possibleAnswers: {
+    value: string;
+    title: string;
+    label: string;
+  }[];
+  rightAnswer: string;
+  hasNextQuestion: boolean;
+}
+
+const TestingPage = () => {
+  const [testId, setTestId] = useState('');
+  const [info, setInfo] = useState<IInfo>();
+
+  useEffect(() => {
+    setInfo({
+      questionId: array[0].questionId,
+      title: array[0].title,
+      rightAnswer: array[0].rightAnswer,
+      possibleAnswers: array[0].possibleAnswers,
+      hasNextQuestion: array[0].hasNextQuestion,
+    });
+    setTestId('cef14478-0352-424f-b3ca-9eec3ceb8e31');
+  }, []);
 
   return (
     <>
       <HeaderTest />
-      <TestQuestion
-        questionId={array[number].questionId}
-        title={array[number].title}
-        rightAnswer={array[number].rightAnswer}
-        possibleAnswers={array[number].possibleAnswers}
-        hasNextQuestion={array[number].hasNextQuestion}
-        onNextQuestion={() => setNumber(number + 1)}
-      />
+      {info && (
+        <TestQuestion
+          testId={testId}
+          questionId={info.questionId}
+          title={info.title}
+          rightAnswer={info.rightAnswer}
+          possibleAnswers={info.possibleAnswers}
+          hasNextQuestion={info.hasNextQuestion}
+          onNextQuestion={setInfo}
+        />
+      )}
     </>
   );
 };
