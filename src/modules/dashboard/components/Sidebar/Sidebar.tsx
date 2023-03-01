45026ac5ee9ebc3bd51d543743bfd71aac762528
Vita Kostiuk <vitagrebennik@gmail.com>
2023-03-01 18:42:45 +0200

    SR-24: add-switch-lang-on-sidebar

--- src/modules/dashboard/components/Sidebar/Sidebar.tsx
+++ src/modules/dashboard/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { useTranslation } from 'react-i18next';
 import { SideBar } from 'ui-kit/SideBar/SideBar';
 import { Menu } from 'ui-kit/Menu/Menu';
 
@@ -12,61 +12,62 @@ function generateId() {
   );
 }
 
-const menu = [
-  {
-    title: 'Dashboard',
-    icon: ICONS.CHART,
-    iconAlt: 'dashboard',
-    path: ROUTES.DASHBOARD,
-  },
-  {
-    title: 'Certification',
-    icon: ICONS.DIPLOMA,
-    iconAlt: 'Certification',
-    path: ROUTES.CERTIFICATION,
-  },
-  {
-    title: 'Petprojects',
-    icon: ICONS.FILES,
-    iconAlt: 'Petprojects',
-    path: ROUTES.PETPROJECTS,
-  },
-  {
-    title: 'Leaderboard',
-    icon: ICONS.TROPHY,
-    iconAlt: 'Leaderboard',
-    path: ROUTES.LEADERBOARD,
-  },
-  {
-    title: 'Vacancies',
-    icon: ICONS.CASE_ROUND,
-    iconAlt: 'Vacancies',
-    path: ROUTES.VACANCIES,
-  },
-];
-
-const userMenu = [
-  {
-    title: 'Profile',
-    icon: ICONS.USER,
-    iconAlt: 'Profile',
-    path: ROUTES.PROFILE,
-  },
-  {
-    title: 'Settings',
-    icon: ICONS.SETTINGS,
-    iconAlt: 'Settings',
-    path: ROUTES.SETTINGS,
-  },
-  {
-    title: 'Feedback',
-    icon: ICONS.MESSAGE_CIRCLE,
-    iconAlt: 'Feedback',
-    path: ROUTES.FEEDBACK,
-  },
-];
-
 export const Sidebar = () => {
+  const { t } = useTranslation();
+
+  const menu = [
+    {
+      title: t('sidebar.dashboard'),
+      icon: ICONS.CHART,
+      iconAlt: 'dashboard',
+      path: ROUTES.DASHBOARD,
+    },
+    {
+      title: t('sidebar.certification'),
+      icon: ICONS.DIPLOMA,
+      iconAlt: 'Certification',
+      path: ROUTES.CERTIFICATION,
+    },
+    {
+      title: t('sidebar.petprojects'),
+      icon: ICONS.FILES,
+      iconAlt: 'Petprojects',
+      path: ROUTES.PETPROJECTS,
+    },
+    {
+      title: t('sidebar.leaderboard'),
+      icon: ICONS.TROPHY,
+      iconAlt: 'Leaderboard',
+      path: ROUTES.LEADERBOARD,
+    },
+    {
+      title: t('sidebar.vacancies'),
+      icon: ICONS.CASE_ROUND,
+      iconAlt: 'Vacancies',
+      path: ROUTES.VACANCIES,
+    },
+  ];
+
+  const userMenu = [
+    {
+      title: t('sidebar.profile'),
+      icon: ICONS.USER,
+      iconAlt: 'Profile',
+      path: ROUTES.PROFILE,
+    },
+    {
+      title: t('sidebar.settings'),
+      icon: ICONS.SETTINGS,
+      iconAlt: 'Settings',
+      path: ROUTES.SETTINGS,
+    },
+    {
+      title: t('sidebar.feedback'),
+      icon: ICONS.MESSAGE_CIRCLE,
+      iconAlt: 'Feedback',
+      path: ROUTES.FEEDBACK,
+    },
+  ];
   return (
     <SideBar
       children={[
