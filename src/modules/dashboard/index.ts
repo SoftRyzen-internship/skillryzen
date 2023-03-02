118f5cbd43d5e4b0d482ba38b789f251fc0fa88c
egarra <garikhabuzov@gmail.com>
2023-03-02 16:26:35 +0200

    SR-45-Sidebar module relocate

--- src/modules/dashboard/index.ts
+++ src/modules/dashboard/index.ts
@@ -3,6 +3,6 @@ export { Layout } from './components/Layout/Layout';
 export { AuthIntro } from './components/AuthIntro/AuthIntro';
 export { Auth } from './components/Auth/Auth';
 export { AuthSteps } from './components/AuthSteps/AuthSteps';
-export { Sidebar } from './components/Sidebar/Sidebar';
+export { Sidebar } from '../Sidebar/Sidebar';
 export { HeaderTest } from './components/HeaderTest/HeaderTest';
 export { MainContainer } from './components/MainContainer/MainContainer';
