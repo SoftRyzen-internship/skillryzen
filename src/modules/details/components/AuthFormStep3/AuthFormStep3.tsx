30e25e10cebe5c8341cf7a3f90ae54577fc2fbc1
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-03-02 11:59:59 +0200

    SR-18: fix input validation styles

--- src/modules/details/components/AuthFormStep3/AuthFormStep3.tsx
+++ src/modules/details/components/AuthFormStep3/AuthFormStep3.tsx
@@ -50,14 +50,17 @@ export const AuthFormStep3 = ({ setStep }: IAuth) => {
       <p className={container.logIn}>Lorem lorem</p>
       <form onSubmit={handleSubmit} className={s.form}>
         {/* ПЕРШИЙ ІНПУТ */}
-        <div className={s.floatingGroup}>
+        <div
+          className={`${s.floatingGroup} ${
+            touched.name &&
+            (errors.name ? s.floatingLabelError : s.floatingLabelValid)
+          }`}
+        >
           {touched.name && errors.name && (
             <p className={s.errorMsg}>{errors.name}</p>
           )}
           <input
-            className={`${s.input} ${
-              touched.name && (errors.name ? s.inputError : s.inputValid)
-            }`}
+            className={s.input}
             name='name'
             type='text'
             id='name'
@@ -67,25 +70,22 @@ export const AuthFormStep3 = ({ setStep }: IAuth) => {
             autoComplete='name'
             placeholder='Name'
           />
-          <label
-            className={`${s.floatingLabel} ${
-              touched.name &&
-              (errors.name ? s.floatingLabelError : s.floatingLabelValid)
-            }`}
-            htmlFor='name'
-          >
+          <label className={s.floatingLabel} htmlFor='name'>
             Name
           </label>
         </div>
         {/* Другий ІНПУТ */}
-        <div className={s.floatingGroup}>
+        <div
+          className={`${s.floatingGroup} ${
+            touched.surname &&
+            (errors.surname ? s.floatingLabelError : s.floatingLabelValid)
+          }`}
+        >
           {touched.surname && errors.surname && (
             <p className={s.errorMsg}>{errors.surname}</p>
           )}
           <input
-            className={`${s.input}  ${
-              touched.surname && (errors.surname ? s.inputError : s.inputValid)
-            }`}
+            className={s.input}
             name='surname'
             type='text'
             id='surname'
@@ -95,26 +95,22 @@ export const AuthFormStep3 = ({ setStep }: IAuth) => {
             autoComplete='surname'
             placeholder='Surname'
           />
-          <label
-            className={`${s.floatingLabel} ${
-              touched.surname && errors.surname
-                ? s.floatingLabelError
-                : s.floatingLabelValid
-            }`}
-            htmlFor='surname'
-          >
+          <label className={s.floatingLabel} htmlFor='surname'>
             Surname
           </label>
         </div>
         {/* Третій ІНПУТ */}
-        <div className={`${s.floatingGroup}`}>
+        <div
+          className={`${s.floatingGroup} ${
+            touched.phone &&
+            (errors.phone ? s.floatingLabelError : s.floatingLabelValid)
+          }`}
+        >
           {touched.phone && errors.phone && (
             <p className={s.errorMsg}>{errors.phone}</p>
           )}
           <InputMask
-            className={`${s.inputPhone}  ${
-              touched.phone && (errors.phone ? s.inputError : s.inputValid)
-            }`}
+            className={s.inputPhone}
             name='phone'
             type='phone'
             mask='+380 99 999 99 99'
@@ -125,13 +121,7 @@ export const AuthFormStep3 = ({ setStep }: IAuth) => {
             autoComplete='off'
             placeholder='Phone'
           />
-          <label
-            className={`${s.floatingLabelPhone} ${
-              touched.phone &&
-              (errors.phone ? s.floatingLabelError : s.floatingLabelValid)
-            }`}
-            htmlFor='phone'
-          >
+          <label className={s.floatingLabelPhone} htmlFor='phone'>
             Phone
           </label>
           <button type='button' className={s.phoneButton}>
