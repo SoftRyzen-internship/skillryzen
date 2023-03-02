ab35193a81f1243059f6fb8a7b1dbecdd7aad9f5
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-03-02 07:51:55 +0200

    SR-18: add language and theme btn

--- src/modules/dashboard/components/Auth/Auth.tsx
+++ src/modules/dashboard/components/Auth/Auth.tsx
@@ -3,6 +3,8 @@ import { useState } from 'react';
 import { AuthIntro, AuthSteps } from 'modules/dashboard';
 
 import s from './Auth.module.scss';
+import { HeaderButtonLanguage } from 'modules/common/components/HeaderButtonList/HeaderButtonLanguage';
+import { HeaderButtonTheme } from 'modules/common/components/HeaderButtonList/HeaderButtonTheme';
 
 export const Auth = () => {
   const [step, setStep] = useState(1);
@@ -10,6 +12,10 @@ export const Auth = () => {
 
   return (
     <main className={s.container}>
+      <div className={s.settings}>
+        <HeaderButtonLanguage />
+        <HeaderButtonTheme />
+      </div>
       {step < 3 && <AuthIntro role={role} />}
       <AuthSteps step={step} setStep={setStep} role={role} setRole={setRole} />
     </main>
