f366a4ec576d192421b4b4ad12dd4d9cc08e928f
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-03-02 18:19:40 +0200

    SR-45: init redux for steps and role

--- src/modules/dashboard/index.ts
+++ src/modules/dashboard/index.ts
@@ -1,6 +1,5 @@
 export { Layout } from './components/Layout/Layout';
 export { AuthIntro } from './components/AuthIntro/AuthIntro';
-export { Auth } from './components/Auth/Auth';
 export { AuthSteps } from './components/AuthSteps/AuthSteps';
 export { Sidebar } from 'modules/Sidebar/Sidebar';
 export { MainContainer } from './components/MainContainer/MainContainer';
