1e43200e0cc94d8a69c19aea5e712f32e815c30c
GubarievaAnna <annagubarieva@gmail.com>
2023-03-03 16:35:37 +0200

    SR-1: Add redux for tests

--- src/modules/common/components/Timer/Timer.tsx
+++ src/modules/common/components/Timer/Timer.tsx
@@ -6,26 +6,34 @@ import { convertTime } from 'utils/convertTime';
 import s from './Timer.module.scss';
 
 interface Timer {
-  time: number;
   theme?: Theme;
 }
 
-export const Timer: React.FC<Timer> = ({ time, theme='dark' }) => {
-  const [seconds, setSeconds] = useState(time * 3600);
+export const Timer: React.FC<Timer> = ({ theme = 'dark' }) => {
+  const [seconds, setSeconds] = useState(0);
 
   useEffect(() => {
-    if (seconds > 0) {
-      const intervalId = setInterval(() => {
-        setSeconds((seconds) => seconds - 1);
-      }, 1000);
-      return () => clearInterval(intervalId);
-    }
+    // if (seconds > 0) {
+    //   const intervalId = setInterval(() => {
+    //     setSeconds((seconds) => seconds - 1);
+    //   }, 1000);
+    //   return () => clearInterval(intervalId);
+    // }
+    const intervalId = setInterval(() => {
+      setSeconds((seconds) => seconds + 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
   }, [seconds]);
 
   return (
     <div>
-      <p className={`${s.timer__text} ${s[`timer__text--${theme}`]}`}>Time left:</p>
-      <p className={`${s.timer__time} ${s[`timer__time--${theme}`]}`}>{convertTime(seconds)}</p>
+      <p className={`${s.timer__text} ${s[`timer__text--${theme}`]}`}>
+        Time left:
+      </p>
+      <p className={`${s.timer__time} ${s[`timer__time--${theme}`]}`}>
+        {convertTime(seconds)}
+      </p>
     </div>
   );
 };
--- src/ui-kit/components/ProgressBar/ProgressBar.tsx
+++ src/ui-kit/components/ProgressBar/ProgressBar.tsx
@@ -3,51 +3,22 @@ import { ICONS } from 'ui-kit/icons';
 import { Theme } from 'modules/common/types';
 
 import s from './ProgressBar.module.scss';
-
-// interface Answer {
-//   number: number;
-//   isRight: boolean;
-// }
+import { useAppSelector } from 'hooks/hook';
 
 interface Props {
-  currentNumber: number;
-  totalNumber: number;
-  isPrevRight: boolean;
   theme?: Theme;
 }
 
-export const ProgressBar = ({
-  currentNumber,
-  totalNumber,
-  isPrevRight,
-  theme = 'dark',
-}: Props) => {
-  const array = createArray(totalNumber);
-
-  // const [arrayOfAnswers, setArrayOfAnswers] = useState<Answer[]>([]);
-
-  // useEffect(() => {
-  //   if (currentNumber === 1) return;
-  //   setArrayOfAnswers((prevState) => [
-  //     ...prevState,
-  //     { number: currentNumber - 1, isRight: isPrevRight },
-  //   ]);
-  // }, [currentNumber, isPrevRight]);
-
-  // const checkRightAnswer = (item: number) => {
-  //   if (item > arrayOfAnswers.length || arrayOfAnswers.length === 0) return '';
-  //   const foundItem = arrayOfAnswers.find((answer) => answer.number === item);
-  //   if (!foundItem) return;
-  //   if (foundItem.isRight) return 'progressBar__line--right';
-  //   return 'progressBar__line--false';
-  // };
+export const ProgressBar = ({ theme = 'dark' }: Props) => {
+  const number = useAppSelector((state) => state.testingInfo.number);
+  const array = createArray(10);
 
   return (
     <div className={s.progressBar}>
       <p
         className={`${s.progressBar__info} ${s[`progressBar__info--${theme}`]}`}
       >
-        Question {currentNumber}/{totalNumber}
+        Question {number}/10
       </p>
       <div className={s.progressBar__wrapper}>
         <ul className={s.progressBar__list}>
@@ -55,11 +26,8 @@ export const ProgressBar = ({
             <li
               key={item}
               className={`${s.progressBar__line} ${
-                item < currentNumber && s['progressBar__line--right']
+                item < number && s['progressBar__line--right']
               }`}
-              // className={`${s.progressBar__line} ${
-              //   s[`${checkRightAnswer(item)}`]
-              // }`}
             ></li>
           ))}
         </ul>
@@ -72,3 +40,32 @@ export const ProgressBar = ({
     </div>
   );
 };
+
+// для демо-версії
+
+// interface Answer {
+//   number: number;
+//   isRight: boolean;
+// }
+
+// const [arrayOfAnswers, setArrayOfAnswers] = useState<Answer[]>([]);
+
+// useEffect(() => {
+//   if (currentNumber === 1) return;
+//   setArrayOfAnswers((prevState) => [
+//     ...prevState,
+//     { number: currentNumber - 1, isRight: isPrevRight },
+//   ]);
+// }, [currentNumber, isPrevRight]);
+
+// const checkRightAnswer = (item: number) => {
+//   if (item > arrayOfAnswers.length || arrayOfAnswers.length === 0) return '';
+//   const foundItem = arrayOfAnswers.find((answer) => answer.number === item);
+//   if (!foundItem) return;
+//   if (foundItem.isRight) return 'progressBar__line--right';
+//   return 'progressBar__line--false';
+// };
+
+// className={`${s.progressBar__line} ${
+//   s[`${checkRightAnswer(item)}`]
+// }`}
