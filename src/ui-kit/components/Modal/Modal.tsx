20ad28054a36eb5fcf475cf6f805d2c6839539d6
Vita Kostiuk <vitagrebennik@gmail.com>
2023-03-06 14:35:36 +0200

    SR-53: add-styles

--- src/ui-kit/components/Modal/Modal.tsx
+++ src/ui-kit/components/Modal/Modal.tsx
@@ -3,10 +3,31 @@ import s from './Modal.module.scss';
 
 interface ModalProps {
   onClick: () => void;
-  text: string;
+  title1?: { text: string; className: string };
+  title2?: { text: string; className: string };
+  subtitle?: { text: string; className: string };
+  link?: { text: string; className: string };
+  image?: {
+    src: string;
+    alt: string;
+    width: string;
+    height: string;
+    className: string;
+  };
+  icon?: JSX.Element;
+  button?: JSX.Element;
 }
 
-export const Modal = ({ onClick, text }: ModalProps) => {
+export const Modal = ({
+  onClick,
+  title1,
+  title2,
+  subtitle,
+  link,
+  image,
+  icon,
+  button,
+}: ModalProps) => {
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
       if (e.code === 'Escape') {
@@ -31,11 +52,31 @@ export const Modal = ({ onClick, text }: ModalProps) => {
         <button type='button' className={s.closeModalBtn} onClick={onClick}>
           {/* <CloseModal /> */}
         </button>
-        <p className={s.text}>{text}</p>
+        {/* <div className={s.imgThumb}> */}
+        <img
+          src={image.src}
+          alt={image.alt}
+          width={image.width}
+          height={image.height}
+          className={image.className}
+        />
+        {/* </div> */}
+        <p className={title1.className}>{title1.text}</p>
+        <p className={title2.className}>{title2.text}</p>
+        <p className={subtitle.className}>
+          {subtitle.text}
+          <span>
+            <a className={link.className} href=''>
+              {link.text}
+            </a>
+          </span>
+        </p>
+
         <div className={s.btnContainer}>
-          <button className={s.btn} type='button' onClick={onClick}>
+          {button}
+          {/* <button className={s.btn} type='button' onClick={onClick}>
             no
-          </button>
+          </button> */}
         </div>
       </div>
     </div>
