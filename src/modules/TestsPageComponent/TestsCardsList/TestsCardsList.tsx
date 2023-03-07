100e72ba2f45ab8657a608291add32b74af8924b
Vita Kostiuk <vitagrebennik@gmail.com>
2023-03-07 16:58:47 +0200

    SR-71: hide-pagination

--- src/modules/TestsPageComponent/TestsCardsList/TestsCardsList.tsx
+++ src/modules/TestsPageComponent/TestsCardsList/TestsCardsList.tsx
@@ -1,18 +1,10 @@
 import { CardsList } from 'modules/common/CardsList/CardsList';
-import { Pagination } from 'ui-kit';
-import { useState } from 'react';
 
 interface TestsListProps {
   size?: 'large' | 'small';
 }
 
 export const TestsCardsList = ({ size }: TestsListProps) => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const totalPages = 10;
-
-  const handlePageChange = (pageNumber: number) => {
-    setCurrentPage(pageNumber);
-  };
   const testsArray = [
     {
       id: 1,
@@ -48,11 +40,5 @@ export const TestsCardsList = ({ size }: TestsListProps) => {
     },
   ];
 
-  // return <CardsList type='info' size={size} testsArray={testsArray} />;
-  return (
-    <>
-      <CardsList type='info' size={size} testsArray={testsArray} />
-      <Pagination totalPages={totalPages} onPageChange={handlePageChange} />
-    </>
-  );
+  return <CardsList type='info' size={size} testsArray={testsArray} />;
 };
