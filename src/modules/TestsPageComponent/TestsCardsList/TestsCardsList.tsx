0ee633e448af841de096d8489cf22e42d020c757
GubarievaAnna <annagubarieva@gmail.com>
2023-03-08 09:33:54 +0200

    SR-3: Delete comments

--- src/modules/TestsPageComponent/TestsCardsList/TestsCardsList.tsx
+++ src/modules/TestsPageComponent/TestsCardsList/TestsCardsList.tsx
@@ -15,33 +15,6 @@ export const TestsCardsList = ({ size }: TestsListProps) => {
       number: 50,
       time: 3,
     },
-    {
-      id: 2,
-      title: 'FullStuck_Final_Test',
-      text: 'Welcome to Star class LMS! Study anytime and anywhere with us and discover the unknown.',
-      fields: ['HTML', 'CSS', 'REACT', 'JAVASCRIPT'],
-      author: 'GoIt',
-      number: 50,
-      time: 3,
-    },
-    {
-      id: 3,
-      title: 'FullStuck_Final_Test',
-      text: 'Welcome to Star class LMS! Study anytime and anywhere with us and discover the unknown.',
-      fields: ['HTML', 'CSS', 'REACT', 'JAVASCRIPT'],
-      author: 'GoIt',
-      number: 50,
-      time: 3,
-    },
-    {
-      id: 4,
-      title: 'FullStuck_Final_Test',
-      text: 'Welcome to Star class LMS! Study anytime and anywhere with us and discover the unknown.',
-      fields: ['HTML', 'CSS', 'REACT', 'JAVASCRIPT'],
-      author: 'GoIt',
-      number: 50,
-      time: 3,
-    },
   ];
 
   return <CardsList size={size} testsArray={testsArray} />;
