b0e4f027b3b49c25bda673b9a5c6270a3417a222
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-03-08 12:06:11 +0200

    SR-74: init change language in scheme validation

---
+++ src/modules/Forms/RegisterAuthForm/useValidationSchema.tsx
@@ -0,0 +1,30 @@
+import * as Yup from 'yup';
+import { useTranslation } from 'react-i18next';
+
+export const useValidationSchema = () => {
+  const { t } = useTranslation();
+
+  return Yup.object({
+    email: Yup.string()
+      .email(t('editPasswordForm.validation.email.invalid'))
+      .required(t('editPasswordForm.validation.email.required'))
+      .max(63, t('editPasswordForm.validation.email.max'))
+      .matches(
+        /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/,
+        t('editPasswordForm.validation.email.regex')
+      )
+      .matches(
+        /^[^.-].*[^.-]$/,
+        t('editPasswordForm.validation.email.startsWithEndsWith')
+      ),
+    password: Yup.string()
+      .min(8, t('editPasswordForm.validation.currentPassword.min'))
+      .test(
+        'has-spaces',
+        t('editPasswordForm.validation.currentPassword.spaces'),
+        (value) => !/\s/.test(value)
+      )
+      .required(t('editPasswordForm.validation.currentPassword.required')),
+    checkbox: Yup.boolean().oneOf([true]).required(),
+  });
+};
--- src/modules/forms/RegisterContactsForm/validationSchema.ts
+++ src/modules/forms/RegisterContactsForm/validationSchema.ts
@@ -1,27 +1,32 @@
 import * as Yup from 'yup';
+import { useTranslation } from 'react-i18next';
 
-export const validationSchema = Yup.object({
-  companyName: Yup.string()
-    .min(2, 'Повинно містити біле 2 символів')
-    .max(30, 'Повинно містити не більше 30 символів'),
-  name: Yup.string()
-    .required('Ім`я є обовязковим')
-    .min(2, 'Повинно містити біле 2 символів')
-    .max(30, 'Повинно містити не більше 50 символів'),
-  surname: Yup.string()
-    .required('Прізвище є обовязковим')
-    .min(2, 'Повинно містити біле 2 символів')
-    .max(30, 'Повинно містити не більше 50 символів'),
-  phone: Yup.string()
-    .transform((value, originalValue) => {
-      return originalValue
-        ? originalValue.replace(/ /g, '').replace(/_/g, '')
-        : '';
-    })
-    .matches(
-      // eslint-disable-next-line
-      /((\+38)\(?\d{3}\)?[\s\.-]?(\d{7}|\d{3}[\s\.-]\d{2}[\s\.-]\d{2}|\d{3}-\d{4}))/,
-      'Невірний формат телефону'
-    )
-    .required('Номер телефону є обов`язковим'),
-});
+export const useValidationSchema = () => {
+  const { t } = useTranslation();
+
+  return Yup.object({
+    companyName: Yup.string()
+      .min(2, t('editProfileForm.validation.companyName.min'))
+      .max(50, t('editProfileForm.validation.companyName.max')),
+    name: Yup.string()
+      .required(t('editProfileForm.validation.name.required'))
+      .min(2, t('editProfileForm.validation.name.min'))
+      .max(30, t('editProfileForm.validation.name.max')),
+    surname: Yup.string()
+      .required(t('editProfileForm.validation.surname.required'))
+      .min(2, t('editProfileForm.validation.surname.min'))
+      .max(30, t('editProfileForm.validation.surname.max')),
+    phone: Yup.string()
+      .transform((value, originalValue) => {
+        return originalValue
+          ? originalValue.replace(/ /g, '').replace(/_/g, '')
+          : '';
+      })
+      .matches(
+      // eslint-disable-next-line no-useless-escape
+        /((\+38)\(?\d{3}\)?[\s\.-]?(\d{7}|\d{3}[\s\.-]\d{2}[\s\.-]\d{2}|\d{3}-\d{4}))/,
+        t('editProfileForm.validation.phone.format')
+      )
+      .required(t('editProfileForm.validation.phone.required')),
+  });
+};
