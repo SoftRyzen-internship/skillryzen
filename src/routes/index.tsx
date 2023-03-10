8762219dda2ec51b7d158cb1149261dbb62382e7
Oleksandr Nikolaienko <aleksander.nikolaenko@gmail.com>
2023-03-10 08:49:45 +0200

    SR-68: Delete comments

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -52,9 +52,9 @@ export const AppRoutes = () => {
         },
         {
           element: (
-            // <ProtectedRoute isLoggedIn={isLoggedIn}>
-            <Outlet />
-            // </ProtectedRoute>
+            <ProtectedRoute isLoggedIn={isLoggedIn}>
+              <Outlet />
+            </ProtectedRoute>
           ),
           children: [
             {
