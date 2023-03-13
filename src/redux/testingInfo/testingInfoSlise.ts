034378c98deedf5fe27b90c562ae12258c8eec7f
GubarievaAnna <annagubarieva@gmail.com>
2023-03-13 14:26:07 +0200

    SR-3: Add session storage

--- src/redux/testingInfo/testingInfoSlise.ts
+++ src/redux/testingInfo/testingInfoSlise.ts
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import storage from 'redux-persist/lib/storage/session';
 
 import { answerTest, finishTest, getTestTemplate} from './testingInfoOperations';
 
