89bc59418430dc4f158b0d2860f7cf746eb05e0f
Kretsul-Vitaliy <turistvital1988@gmail.com>
2023-03-13 18:08:32 +0200

    SR:84-feedback-clean-optimise

--- src/redux/authSlice/authSelectors.js
+++ src/redux/authSlice/authSelectors.js
@@ -1,3 +1,4 @@
 export const getUserEmail = (state) => state.auth.user.email;
 export const getUserRole = (state) => state.auth.user.role;
+export const getUserName = (state) => state.auth.user.displayName;
 export const getStep = (state) => state.auth.step;
