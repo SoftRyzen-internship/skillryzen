11c112af0344b7002266c79aeea5af2121376d5c
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2023-03-22 17:34:43 +0200

    SR-103: Init ScrollContainer

---
+++ src/ui-kit/components/ScrollContainer/ScrollContainer.tsx
@@ -0,0 +1,9 @@
+import s from './Container.module.scss';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const ScrollContainer = ({ children }: Props) => {
+  return <div className={s.wrapper}>{children}</div>;
+};
