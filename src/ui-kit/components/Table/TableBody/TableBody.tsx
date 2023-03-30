1d81903e5c4290e2e6129147f60f5135068181b9
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2023-03-30 18:19:27 +0300

    SR-27: Refactor Table to TableHead and TableBody

---
+++ src/ui-kit/components/Table/TableBody/TableBody.tsx
@@ -0,0 +1,37 @@
+import { IThemeContext } from 'constans/types';
+import { useThemeContext } from 'context/themeContext';
+
+import { Column } from '../Table';
+
+import s from './TableBody.module.scss';
+
+interface TableBodyProps<T> {
+  columns: Column<T>[];
+  className: string;
+  sortedData: T[];
+}
+
+export const TableBody = <T extends { id: string }>({
+  columns,
+  className,
+  sortedData,
+}: TableBodyProps<T>) => {
+  const { theme }: IThemeContext = useThemeContext();
+
+  return (
+    <tbody>
+      {sortedData.map(item => (
+        <tr
+          key={String(item.id)}
+          className={`${s.tablerow} ${s[`tablerow--${theme}`]} ${className}`}
+        >
+          {columns.map(column => (
+            <td key={column.property as string} className={s.data}>
+              {item[column.property]?.toString()}
+            </td>
+          ))}
+        </tr>
+      ))}
+    </tbody>
+  );
+};
